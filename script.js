5fa625361e318e2654d544ac8c1da87332a33821
MatějDvorak <matej.dvorak.07@seznam.cz>
2025-01-21 20:01:04 +0100

    Verze.1

---
+++ script.js
@@ -0,0 +1,13 @@
+let x = document.getElementById("tbx");
+let y = document.getElementById("tby");
+let tla = document.getElementById("tla");
+let odpoved = document.getElementById("odpoved");
+
+function square(x, y) {
+
+}
+btn.addEventListener("click", function() {
+    let x = Number(xInput.value);
+    let y = Number(yInput.value);
+    square(x, y);
+});
